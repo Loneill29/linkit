663079e6123d78ae2f4b2a61168f70aa6a350905
Linda <oneill.linda21@gmail.com>
2018-10-20 15:06:04 -0500

    add user model and sign-up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Linkit User", email: "user@email.com", password: "password") }
 
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
