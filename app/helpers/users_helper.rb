414e44952fb72cfb3d3e2c291a11830137436120
Linda <oneill.linda21@gmail.com>
2018-10-22 14:00:04 -0500

    add comment and vote factories

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,25 @@
 module UsersHelper
+  def user_has_comments?
+    if @user.comments.any?
+      @user.comments
+    else
+      "#{@user.name} has not written any comments yet."
+    end
+  end
+
+  def user_has_posts?
+    if @user.posts.any?
+      @user.posts
+    else
+      "#{@user.name} has not written any posts yet."
+    end
+  end
+
+  def user_has_favorites?
+    if @user.favorites.any?
+      @user.favorites
+    else
+      "#{@user.name} has not favorited any posts yet."
+    end
+  end 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,16 +4,17 @@ RSpec.describe Comment, type: :model do
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
   let(:post) { create(:post) }
-  let(:comment) { Comment.create!(body: 'Comment body', post: post, user: user) }
+  let(:comment) { create(:comment) }
 
-    it { is_expected.to belong_to(:post) }
-    it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:post) }
+  it { is_expected.to belong_to(:user) }
 
-    it { is_expected.to validate_presence_of(:body) }
-    it { is_expected.to validate_length_of(:body).is_at_least(5) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_length_of(:body).is_at_least(5) }
+
   describe "attributes" do
     it "has a body attribute" do
-      expect(comment).to have_attributes(body: "Comment body")
+      expect(comment).to have_attributes(comment: comment.body)
     end
   end
 
