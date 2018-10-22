cc3f8822ef378eecbb1daeecfc41adaa31839b40
Linda <oneill.linda21@gmail.com>
2018-10-22 12:04:50 -0500

    add up/down vote

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,13 +20,17 @@ topics = Topic.all
 
 #create posts
 50.times do
-  Post.create!(
+  post = Post.create!(
     user:   users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
+
 posts = Post.all
 
 #create sponsored posts
@@ -97,3 +101,4 @@ puts "#{Comment.count} comments created"
 puts "#{Advertisement.count} ads created"
 puts "#{Question.count} questions created"
 puts "#{Topic.count} topics created"
+puts "#{Vote.count} votes created"
